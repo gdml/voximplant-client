3d880f1b60a964f54df34039dbba79a38d8133c1
Alexey Chudin <kazqvaizer@gmail.com>
2020-03-05 18:29:37 +1000

    Bumped version in setup py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='voximplant_client',
-    version='0.0.2',
+    version='0.0.3',
     description="Voximplant.com API client.",
     keywords=[],
     url="https://github.com/gdml/voximplant-client/",
